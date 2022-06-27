7abf54fdccb33ba8f7677b6d3d70872b41f25e40
Robin Huy <12640832+robinhuy@users.noreply.github.com>
2022-06-27 14:14:27 +0700

    update formidable

--- additional_routes.js
+++ additional_routes.js
@@ -1,19 +1,22 @@
-const formidable = require('formidable');
-const {copyFile, unlink} = require('fs/promises');
-const {generateJwtToken} = require('./jwt-authenticate');
+const formidable = require("formidable");
+const { copyFile, unlink } = require("fs/promises");
+const { generateJwtToken } = require("./jwt-authenticate");
 
 const handleUploadFile = async (req, file) => {
-  const uploadFolder = 'uploads';
+  const uploadFolder = "uploads";
 
   try {
     // Copy file from temp folder to uploads folder (not rename to allow cross-device link)
-    await copyFile(file.path, `./public/${uploadFolder}/${file.name}`);
+    await copyFile(
+      file.filepath,
+      `./public/${uploadFolder}/${file.originalFilename}`
+    );
 
     // Remove temp file
-    await unlink(file.path);
+    await unlink(file.filepath);
 
     // Return new path of uploaded file
-    file.path = `${req.protocol}://${req.get('host')}/${uploadFolder}/${
+    file.filepath = `${req.protocol}://${req.get("host")}/${uploadFolder}/${
       file.name
     }`;
 
@@ -25,77 +28,77 @@ const handleUploadFile = async (req, file) => {
 
 module.exports = {
   loginHandler: (db, req, res) => {
-    const {username, email, password: pwd} = req.body;
+    const { username, email, password: pwd } = req.body;
 
     const user = db
-      .get('users')
+      .get("users")
       .find(
         (u) =>
-          (u.username === username || u.email === email) && u.password === pwd,
+          (u.username === username || u.email === email) && u.password === pwd
       )
       .value();
 
     if (user && user.password === pwd) {
       const token = generateJwtToken(user.id);
-      const {password, ...userWithoutPassword} = user;
+      const { password, ...userWithoutPassword } = user;
 
       res.jsonp({
         ...userWithoutPassword,
         token,
       });
     } else {
-      res.status(400).jsonp({message: 'Username or password is incorrect!'});
+      res.status(400).jsonp({ message: "Username or password is incorrect!" });
     }
   },
 
   registerHandler: (db, req, res) => {
-    const {username, email, password} = req.body;
+    const { username, email, password } = req.body;
 
     if (!password && (!email || !username)) {
-      res.status(400).jsonp({message: 'Please input all required fields!'});
+      res.status(400).jsonp({ message: "Please input all required fields!" });
       return;
     }
 
     const existUsername = db
-      .get('users')
+      .get("users")
       .find((user) => username && user.username === username)
       .value();
 
     if (existUsername) {
       res.status(400).jsonp({
         message:
-          'The username already exists. Please use a different username!',
+          "The username already exists. Please use a different username!",
       });
       return;
     }
 
     const existEmail = db
-      .get('users')
+      .get("users")
       .find((user) => email && user.email === email)
       .value();
 
     if (existEmail) {
       res.status(400).jsonp({
         message:
-          'The email address is already being used! Please use a different email!',
+          "The email address is already being used! Please use a different email!",
       });
       return;
     }
 
-    const lastUser = db.get('users').maxBy('id').value();
+    const lastUser = db.get("users").maxBy("id").value();
     const newUserId = parseInt(lastUser.id, 10) + 1;
-    const newUser = {id: newUserId, ...req.body};
+    const newUser = { id: newUserId, ...req.body };
 
-    db.get('users').push(newUser).write();
+    db.get("users").push(newUser).write();
 
     res.jsonp(newUser);
   },
 
   uploadFileHandler: (req, res) => {
-    if (req.headers['content-type'] === 'application/json') {
+    if (req.headers["content-type"] === "application/json") {
       res
         .status(400)
-        .jsonp({message: 'Content-Type "application/json" is not allowed.'});
+        .jsonp({ message: 'Content-Type "application/json" is not allowed.' });
       return;
     }
 
@@ -105,7 +108,7 @@ module.exports = {
       let file = files.file;
 
       if (error || !file) {
-        res.status(400).jsonp({message: 'Missing "file" field.'});
+        res.status(400).jsonp({ message: 'Missing "file" field.' });
         return;
       }
 
@@ -114,26 +117,26 @@ module.exports = {
         res.jsonp(file);
       } catch (err) {
         console.log(err);
-        res.status(500).jsonp({message: 'Cannot upload file.'});
+        res.status(500).jsonp({ message: "Cannot upload file." });
       }
     });
   },
 
   uploadFilesHandler: (req, res) => {
-    if (req.headers['content-type'] === 'application/json') {
+    if (req.headers["content-type"] === "application/json") {
       res
         .status(400)
-        .jsonp({message: 'Content-Type "application/json" is not allowed.'});
+        .jsonp({ message: 'Content-Type "application/json" is not allowed.' });
       return;
     }
 
-    const form = formidable({multiples: true});
+    const form = formidable({ multiples: true });
 
     form.parse(req, async (error, fields, files) => {
       let filesUploaded = files.files;
 
       if (error || !filesUploaded) {
-        res.status(400).jsonp({message: 'Missing "files" field.'});
+        res.status(400).jsonp({ message: 'Missing "files" field.' });
         return;
       }
 
@@ -152,13 +155,13 @@ module.exports = {
             } catch (err) {
               throw err;
             }
-          }),
+          })
         );
 
         res.jsonp(filesUploaded);
       } catch (err) {
         console.log(err);
-        res.status(500).jsonp({message: 'Cannot upload files.'});
+        res.status(500).jsonp({ message: "Cannot upload files." });
       }
     });
   },
